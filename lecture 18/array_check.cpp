fef6dd43d5c1cecb83eeab32e2b96cda8d915c42
pragya-kumre <pragyakumre69@gmail.com>
2026-02-10 14:01:44 +0530

    find_value in array

---
+++ lecture 18/array_check.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<climits>
+using namespace std;
+
+int main() {
+ int n;
+ cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+int k;
+cin>>k;
+int ans;
+for(int i=0;i<n;i++){
+    if(arr[i]==k){   
+        cout<<i+1;
+    }
+}
+
+return 0;
+}
