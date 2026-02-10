1ff001d5628662f63174a7a8e1592e269f61e230
pragya-kumre <pragyakumre69@gmail.com>
2026-02-10 14:44:44 +0530

    vector

--- lecture 18/vector.cpp
+++ lecture 18/vector.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<algorithm>
 #include<vector>
 using namespace std;
 
@@ -11,11 +12,39 @@ int main() {
     a.push_back(4);
     a.push_back(5);
     a.pop_back();
+    //size of array
+    cout<<a.size()<<endl;
+    cout<<a.capacity()<<endl;// capacity of vector kitni jagha gher rha hai
     for(int i=0;i<a.size();i++){
         cout<<a[i]<<" ";
     }
     cout<<endl;
     cout<<a.front()<<endl;
     cout<<a.back()<<endl;
+
+    //reverse
+    reverse(a.begin(), a.end());
+   // cout<<a[i]<<" "<<endl;
+    //sort
+    sort(a.begin(),a.end());
+   // cout<<a[i]<<" "<<endl;
+
+    //insert
+    a.insert(a.begin()+2,6);
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    //from end
+      a.insert(a.end()-1,6);
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+
+    cout<<endl;
+    int n=5;
+    vector<int> arr(n,1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
  return 0;
 }
