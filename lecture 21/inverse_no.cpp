93ac96f20aa64d34eb6a7d7c9ee145576ccdc709
pragya-kumre <pragyakumre69@gmail.com>
2026-02-17 23:38:07 +0530

    inverse number

--- lecture 21/inverse_no.cpp
+++ lecture 21/inverse_no.cpp
@@ -5,23 +5,35 @@ vector<int>convert(int n){
     vector<int>number;
     while(n!=0){
         int k=n%10;
-        number.push_back(k);
+        number.push_back(k); // 54213
         n=n/10;
     }
     return number;
 }
 int main() {
-    int n;
+    int n; //31245
     cin>>n;
-    vector<int> number=convert(n);
+    vector<int> number=convert(n);  //[54213]
     vector <int> ans(number.size()+1);
-    for(int i=0;i<number.size();i++){
+    for(int i=0;i<number.size();i++){   //[_,12534]
      ans[number[i]]=i+1;
 
     }
     for(int i=number.size();i>=1;i--){
-        cout<<ans[i];
+        cout<<ans[i];  //12534
     }
  
  return 0;
 }
+
+/*number are swaping with their position example-number 31245
+                                        position-       54321
+                            numbers are becoming postion & position number  
+                                        31245  ↰
+                                           ↪ 54321  
+                          P   N  P   N  P  N   P   N  P   N      (p->position ,N->number)
+                so now in 5-->1, 4-->2, 3-->5, 2-->3, 1-->4
+                             o/p  --->  12534
+                         position -->   ↪54321
+
+*/
