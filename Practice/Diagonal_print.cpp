5743ed56ae3d346fc5e3e575aa7b2f852c02d082
pragya-kumre <pragyakumre69@gmail.com>
2026-03-21 10:25:18 +0530

    assignment

--- Practice/Diagonal_print.cpp
+++ Practice/Diagonal_print.cpp
@@ -13,30 +13,15 @@ int main() {
             cin >> arr[i][j];
     }
 
-    int mainSum = 0, secSum = 0;
-
-    // for(int i = 0; i < n && i < m; i++){
-    //     mainSum += arr[i][i];        // main diagonal
-    //     secSum += arr[i][m-1-i];     // secondary diagonal
-    // }
-    for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-        if(i==j) mainSum += arr[i][j];         // main diagonal
-        if(i+j==m-1) secSum += arr[i][j];   // secondary diagonal
-    }
-}
-
+ int mainSum=0 , secSum=0;
+ int size= (n<m)?n:m;//largest square possible
+ for(int i=0;i<size;i++){
+    mainSum+=arr[i][i];
+    secSum+=arr[i][size-1-i];
+ }
     cout << "Main diagonal sum: " << mainSum << endl;
     cout << "Secondary diagonal sum: " << secSum << endl;
 
     return 0;
 }
 
-/*
-for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-        if(i==j) sum += arr[i][j];         // main diagonal
-        if(i+j==m-1) sum2 += arr[i][j];   // secondary diagonal
-    }
-}
-*/
--- Practice/transpose2D.cpp
+++ Practice/transpose2D.cpp
@@ -11,16 +11,16 @@ int main() {
         cin>>arr[i][j];
     }
  }
- 
+ int transpose[m][n];
 for(int i=0;i<n;i++){
-    for(int j=i+1;j<m;j++){
-        swap(arr[i][j],arr[j][i]);
+    for(int j=0;j<m;j++){
+        transpose [j][i]=arr[i][j];
     }  
  }
 
-for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-        cout<<arr[i][j]<<" ";
+for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        cout<<transpose[i][j]<<" ";
     }  
     cout<<endl;
  }
