2201a8e1a418a909b91dda6f49ba384fe9457286
pragya-kumre <pragya.kumre01@gmail.com>
2026-03-22 10:50:24 +0530

    string practice

--- Practice/concatenation.cpp
+++ Practice/concatenation.cpp
@@ -6,7 +6,7 @@ int main() {
   string s1,s2;
   getline(cin,s1);
   getline(cin,s2);
-  string result = s1+ s2; //s1.append(s2)
+  string result =s1.append(s2);
 
   cout<<result<<" ";
  return 0;
--- Practice/string.cpp
+++ Practice/string.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 int main() {
-  string s1,s2;
+  // string s1,s2;
 
-    cin>>s1;
-    cin.ignore();
-    getline(cin,s2);
+  //   cin>>s1;
+  //   cin.ignore();
+  //   getline(cin,s2);
   
-  cout<<s1<<" "<<s2<<" ";
+  string s1;
+  getline(cin, s1);
+  
+  cout<<s1<<" ";
  return 0;
 }
