4b5f154a33b1d2874f0fa9599f7d3ba97e9e1efc
pragya-kumre <pragya.kumre01@gmail.com>
2026-04-20 19:03:39 +0530

    Armstrong no

--- lecture 19/Armstrong_no.cpp
+++ lecture 19/Armstrong_no.cpp
@@ -28,3 +28,35 @@ int main() {
     }
  return 0;
 }
+/*
+An Armstrong number is a number that is equal to the sum of its digits raised to the power of the number of digits.
+
+Here, each digit d is taken and used like:
+d^k where k = number of digits in the number.
+
+If the sum of all such values equals the original number, it is an Armstrong number.
+
+Example 1
+Input:153
+Digits: 1, 5, 3
+Number of digits k = 3
+
+Step-by-step:
+   1³ = 1
+   5³ = 125
+   3³ = 27
+
+Sum: 1 + 125 + 27 = 153
+
+Output: Armstrong number ✔
+
+Example 2
+Input: 123
+    1³ = 1
+    2³ = 8
+    3³ = 27
+
+Sum: 1 + 8 + 27 = 36
+
+Output: Not an Armstrong number ✘
+*/
