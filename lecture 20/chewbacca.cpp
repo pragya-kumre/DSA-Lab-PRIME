ec52f4f3b513000258ba36e30e75ff137d319668
pragya-kumre <pragya.kumre01@gmail.com>
2026-04-20 18:58:34 +0530

    chewbacca number

--- lecture 20/chewbacca.cpp
+++ lecture 20/chewbacca.cpp
@@ -43,3 +43,26 @@ int main() {
  cout<<reverse(rev)/10<<endl;
  return 0;
 }
+/*
+A Chewbacca number is the smallest possible number formed by replacing each digit d of a given number with min(d, 9 − d),
+ where d represents each individual digit of the number,
+ and the first digit is not allowed to become 0 (if it becomes 0, we keep it as 9 or keep the original digit).
+ Example 1
+
+Input:4545
+Step-by-step:
+          d = 4 → min(4, 5) = 4
+          d = 5 → min(5, 4) = 4
+          d = 4 → 4
+          d = 5 → 4
+
+Output: 4444
+
+Example 2
+Input:909
+Step-by-step:
+         d = 9 → 0 (but first digit cannot become 0, so keep 9)
+         d = 0 → 0
+         d = 9 → 0
+ Output:909
+*/
