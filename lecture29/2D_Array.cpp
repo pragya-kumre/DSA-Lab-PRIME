30c2b3722384a65324ed35182f4be1fcbdf7e52b
pragya-kumre <pragya.kumre01@gmail.com>
2026-04-26 18:27:57 +0530

    2d array

--- lecture29/2D_Array.cpp
+++ lecture29/2D_Array.cpp
@@ -2,9 +2,66 @@
 using namespace std;
 
 int main() {
- int array[] [4]={
-    {1,2,3,4},{5,6,7,8},{9,10,11,12}
- } ;
-   
- return 0;
+    int rows, cols;
+
+    // Input size
+    cout << "Enter number of rows and columns: ";
+    cin >> rows >> cols;
+
+    int arr[100][100]; // fixed size for beginners
+
+    // Input elements
+    cout << "Enter elements of the matrix:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // Display matrix
+    cout << "\nMatrix is:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Sum of all elements
+    int sum = 0;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            sum += arr[i][j];
+        }
+    }
+    cout << "\nSum of elements = " << sum << endl;
+
+    // Search an element
+    int key, found = 0;
+    cout << "\nEnter element to search: ";
+    cin >> key;
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            if(arr[i][j] == key) {
+                cout << "Found at position (" << i << ", " << j << ")\n";
+                found = 1;
+            }
+        }
+    }
+
+    if(!found) {
+        cout << "Element not found\n";
+    }
+
+    // Transpose of matrix
+    cout << "\nTranspose of matrix:\n";
+    for(int j = 0; j < cols; j++) {
+        for(int i = 0; i < rows; i++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
