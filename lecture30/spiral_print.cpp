02cfc9551676645983245d5085e9b7094cc55492
pragya-kumre <pragya.kumre01@gmail.com>
2026-04-26 23:19:25 +0530

    spiral matrix

--- lecture30/spiral_print.cpp
+++ lecture30/spiral_print.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int r,c;
-    cin>>r>>c;
+    cin>>r>>c; 
     vector<vector<int>> arr(r,vector<int>(c));
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
