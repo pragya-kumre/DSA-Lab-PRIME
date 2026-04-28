d22c6ef431038d47fef6d059e5a69f4ec1f2f667
pragya-kumre <pragya.kumre01@gmail.com>
2026-04-28 12:47:25 +0530

    wave rowwise

--- Assignment_5.cpp/Array_wave_Rowwise.cpp
+++ Assignment_5.cpp/Array_wave_Rowwise.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 using namespace std;
 
-int main() {
+int main() {  // print wave rowwise
  int n,m;
  cin>>n>>m;
+
 vector<vector<int>> arr(n,vector<int>(m));
  for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
@@ -22,7 +23,6 @@ vector<vector<int>> arr(n,vector<int>(m));
            for(int j=m-1;j>=0;j--){
             cout<<arr[i][j]<<", ";
            } 
-          
         }
 
     }
