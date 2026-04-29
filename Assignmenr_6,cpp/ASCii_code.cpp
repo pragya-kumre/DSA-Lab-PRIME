78c03b0c8f09ffd117f38413efac39bdf860fdde
pragya-kumre <pragya.kumre01@gmail.com>
2026-04-29 22:22:54 +0530

    notes

--- Assignmenr_6,cpp/ASCii_code.cpp
+++ Assignmenr_6,cpp/ASCii_code.cpp
@@ -7,7 +7,7 @@ int main() {
     cin>>s;
     cout<<s[0];
     for (int i = 1; i < s.length(); i++) {
-        int diff = s[i] - s[i - 1]; // difference in ASCII values
+        int diff = s[i] - s[i - 1];    // Difference in ASCII values
         cout <<diff<< s[i];
     }
  return 0;
