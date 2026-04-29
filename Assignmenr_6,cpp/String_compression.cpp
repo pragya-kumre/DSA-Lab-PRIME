a3f4478f13b163ba0ae941c9023a64d77bdac810
pragya-kumre <pragya.kumre01@gmail.com>
2026-04-29 22:26:48 +0530

    NOTES

--- Assignmenr_6,cpp/String_compression.cpp
+++ Assignmenr_6,cpp/String_compression.cpp
@@ -7,7 +7,6 @@ int main() {
     getline(cin, s);
 
     int count = 1;
-
     for(int i = 0; i < s.length(); i++) {
         if(i < s.length() - 1 && s[i] == s[i+1]) {
             count++;
@@ -15,7 +14,6 @@ int main() {
             cout << s[i] ;
             count = 1;
         }
-    
     }
     return 0;
 }
