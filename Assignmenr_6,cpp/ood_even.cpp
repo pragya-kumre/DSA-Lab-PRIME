1b00a1ed0675b40caf812038161e283fa76ad8ef
pragya-kumre <pragya.kumre01@gmail.com>
2026-04-30 19:31:13 +0530

    assignment

--- Assignmenr_6,cpp/ood_even.cpp
+++ Assignmenr_6,cpp/ood_even.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 void modify(string &s) {
     for(int i = 0; i < s.length(); i++) {
-        if(i % 2 == 0) {      // Even index → next higher char
+        if(i % 2 == 0) {      // even index → next higher char
             s[i] = s[i] + 1;
-        } else {               // Odd index → next lower char
+        } else {               // odd index → next lower char
             s[i] = s[i] - 1;
         }
     }
