d1c0c7833d69ae2e267e0558805660784553ba4c
pragya-kumre <pragya.kumre01@gmail.com>
2026-04-30 19:32:46 +0530

    bubble sort

--- Practice/Bubble_sort.cpp
+++ Practice/Bubble_sort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 void bubbleSort(int arr[], int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
@@ -9,6 +10,7 @@ void bubbleSort(int arr[], int n){
         }
     }
 }
+
 int main() {
 int n;
 cin>>n;
